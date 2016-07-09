9733e2b891195d8cdc0e6fadc4689d0871ebc320
Benjamin Daschel <coffeemaxed@gmail.com>
2016-07-09 01:34:11 -0700

    half working knob rotation

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,49 @@
 
     NodeList.prototype.forEach = HTMLCollection.prototype.forEach = Array.prototype.forEach;
 
+    Math.radiansToDegrees = function (radians) {
+        return radians * (180/Math.PI);
+    };
+
+    var Vector = function (x, y) {
+        this.x = x;
+        this.y = y;
+    };
+
+    /**
+     *
+     * @param vector {Vector}
+     * @returns {Vector}
+     */
+    Vector.prototype.offsetFromHere = function (vector) {
+        // reverse y to switch "graphics" coords to normal cartesian
+        return new Vector(vector.x - this.y, this.y - vector.y);
+    };
+
+    Object.defineProperty(Vector.prototype, 'length', {
+        get: function () {
+            return Math.sqrt(
+                Math.pow(this.x, 2) + Math.pow(this.y, 2)
+            );
+        }
+    });
+
+    Object.defineProperty(Vector.prototype, 'angle', {
+        get: function () {
+            return Math.atan2(this.x, this.y)
+        }
+    });
+
+    function Knob (element) {
+        this.element = element;
+        this.activated = false;
+        this.center = new Vector(element.offsetWidth, element.offsetHeight);
+        element.addEventListener("mousemove", this.drag.bind(this));
+        element.addEventListener('mousedown', this.activate);
+        element.addEventListener('mouseup', this.deactivate);
+        element.addEventListener('mouseleave', this.deactivate);
+    }
+
     Knob.prototype.deactivate = function(e) {
         this.active = false;
         console.log('deactivated');
@@ -17,22 +60,13 @@
     };
 
     Knob.prototype.drag = function(e) {
-        var x = e.clientX,
-            y = e.clientY;
-
-        console.log("X: " + x + ", Y: " + y);
+        var mouseAbsPosition = new Vector(e.clientX, e.clientY),
+            offset = this.center.offsetFromHere(mouseAbsPosition);
+        var degrees = Math.radiansToDegrees(offset.angle);
+        console.log(offset.length);
+        console.log(offset.angle, degrees);
+        this.element.style.transform = "rotate(" + degrees + "deg)";
     };
-
-    function Knob (element) {
-        this.element = element;
-        this.activated = false;
-
-        element.addEventListener("mousemove", this.drag);
-        element.addEventListener('mousedown', this.activate);
-        element.addEventListener('mouseup', this.deactivate);
-        element.addEventListener('mouseleave', this.deactivate);
-    }
-
     var knobs = document.getElementsByClassName("dial");
 
     knobs.forEach(function(element, index) {
